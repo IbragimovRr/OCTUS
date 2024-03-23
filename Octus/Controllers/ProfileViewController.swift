0e0e23c855e6c736f997a69684af06fca46821dc
Eldarik25 <Fot1380123@mail.ru>
2024-03-24 02:02:15 +0300

    2

---
+++ Octus/Controllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Octus
+//
+//  Created by Ибрагимов Эльдар on 24.03.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
